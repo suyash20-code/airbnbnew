e9ce521416ed32a10e37de6a3a5c2604563eacf2
suyash20-code <suyashpadwal10@gmail.com>
2023-09-25 18:35:34 +0530

    monda

--- pages/search.js
+++ pages/search.js
@@ -12,17 +12,17 @@ function Search() {
   // ES6 Destructring
   const { location, startDate, endDate, noOfGuests } = router.query;
 
-  const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
-  const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
-  const range = `${formattedStartDate} - ${formattedEndDate}`;
+  // const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+  // const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+  // const range = `${formattedStartDate} - ${formattedEndDate}`;
 
   return (
     <div className="">
-      <Header placeholder={`${location} | ${range} | ${noOfGuests}`}></Header>
+      <Header placeholder={`${location}  | ${noOfGuests}`}></Header>
       <main className="flex">
         <section className="flex-grow pt-14 px-6">
           <p className="text-xs">
-            300+ Stays {range} for {noOfGuests} guests
+            300+ Stays for {noOfGuests} guests
           </p>
           <h1 className="text-3xl font-semibold mt-2 mb-6">
             Stays in {location}
